160119c08541e69cd8fe61500b290e84e687e950
lconnell1000 <lconnell1000@gmail.com>
2022-01-11 23:25:10 +1030

    some peudo code and styling added

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 var today = moment().format("dddd, MMM Do");
 $("#currentDay").text(today);
+
+
+//when click on middle column on row can edit content inside to add event
+
+//save button at end of row in 3rd column will save content in 2nd column to 
+//local storage when clicked
+
+//data to be displayed the next time the page is shown
+
+//each row is color coded depending wether in the future(green) 
+// current (red) or past(grey)
