12725767e32ccd6da45ffbd36203e3f876c6059c
lconnell1000 <lconnell1000@gmail.com>
2022-01-12 00:50:19 +1030

    trying to add forms but stuffed up bootstrap for first row somehow

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ $("#currentDay").text(today);
 
 
 //when click on middle column on row can edit content inside to add event
+//make form show when user clicks in 2nd column
 
+//create function to handle form submittion
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    var userInput = $('input(name="user-input').val();
+}
 //save button at end of row in 3rd column will save content in 2nd column to 
 //local storage when clicked
 
