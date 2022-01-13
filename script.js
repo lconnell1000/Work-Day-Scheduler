e0ac3e24236363280b0b55b3679995be842039d5
lconnell1000 <lconnell1000@gmail.com>
2022-01-13 17:12:58 +1030

    added in timeColor function to change the colors of the blocks

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var newEventEl = $(".new-event");
+var btnSave = $(".saveBtn")
+var currentTime = moment().format("HH")
+
 var today = moment().format("dddd, MMM Do");
 $("#currentDay").text(today);
 
@@ -6,11 +10,6 @@ $("#currentDay").text(today);
 //make form show when user clicks in 2nd column
 
 //create function to handle form submittion
-function handleFormSubmit(event) {
-    event.preventDefault();
-
-    var userInput = $('input(name="user-input').val();
-}
 //save button at end of row in 3rd column will save content in 2nd column to 
 //local storage when clicked
 
@@ -18,3 +17,28 @@ function handleFormSubmit(event) {
 
 //each row is color coded depending wether in the future(green) 
 // current (red) or past(grey)
+var timeColor = function changeColor () {
+    console.log(newEventEl);
+    //loop through each text area
+    for (var i = 0; i < newEventEl.length; i++){
+        //make a string of newEventEl
+        var newEventTime = newEventEl[i].id;
+
+        //now access the element via the ID in html
+        var timeColor = document.getElementById(newEventTime)
+
+        //remove any previous classes associated with the time
+        $(newEventTime).removeClass(".gone .now .upcoming");
+
+        if (newEventTime > currentTime) {
+            $(timeColor).addClass("upcoming")
+        } else if (newEventTime < currentTime) {
+            $(timeColor).addClass("gone")
+        } else {
+            $(timeColor).addClass("now")
+        }
+    }
+}
+
+//check every minute to see if color should change
+setInterval(timeColor(), (6000))
